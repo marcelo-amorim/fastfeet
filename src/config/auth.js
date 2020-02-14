85c3c21f8d487d08e01299f08631e6c484f85471
Marcelo Amorim <marcelo.brasil.amorim@gmail.com>
2020-02-14 18:02:29 -0300

    first commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b6cd6ac8a56933889b4fe3876b8282a3',
+  expiresIn: '7d',
+};
---
+++ src/server.js
@@ -0,0 +1,6 @@
+import app from './app';
+
+/**
+ * runs server on specified port
+ */
+app.listen(3333);
