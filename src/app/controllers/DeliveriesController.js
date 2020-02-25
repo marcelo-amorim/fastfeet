55c90a9f77bf94dd32590a3fb7d2868a70294854
Marcelo Amorim <marcelo.brasil.amorim@gmail.com>
2020-02-25 01:33:16 -0300

    adds delivery update

--- src/app/controllers/DeliveriesController.js
+++ src/app/controllers/DeliveriesController.js
@@ -110,7 +110,7 @@ class DeliveriesController {
       return res
         .status(400)
         .json(
-          `Deliveryman ${deliveryman.name} has reached the limit of deliveries of the day`
+          `Deliveryman ${deliveryman.name} has reached the limit of deliveries ${maximumDeliveries} of the day`
         );
     }
 
@@ -136,7 +136,120 @@ class DeliveriesController {
   }
 
   async update(req, res) {
-    return res.json();
+    const { id } = req.params;
+
+    if (!id) return res.status(400).json({ error: 'Delivery id not received' });
+
+    const deliverySchema = Yup.object().shape({
+      product: Yup.string(),
+      start_date: Yup.date(),
+      end_date: Yup.date(),
+      recipient_id: Yup.string(),
+      deliveryman_id: Yup.string(),
+      signature_id: Yup.string(),
+    });
+
+    await deliverySchema.validate(req.body).catch(err => {
+      return res.status(400).json({ error: err.errors });
+    });
+
+    const delivery = await Deliveries.findByPk(id);
+    if (!delivery) {
+      return res.status(404).json({ error: 'Delivery not found' });
+    }
+
+    const { start_date, end_date, deliveryman_id } = req.body;
+
+    /**
+     * Checks for start_date and validate it to the current date
+     */
+    if (start_date) {
+      const requestedDate = parseISO(start_date);
+      const today = new Date();
+      /**
+       * Check for past dates
+       */
+      const hourStart = startOfHour(requestedDate);
+      if (isBefore(hourStart, today)) {
+        return res.status(400).json({ error: 'Past dates are not permitted.' });
+      }
+
+      const { OFFICE_HOUR_START } = process.env;
+      const [ofshour, ofsminute] = OFFICE_HOUR_START.split(':');
+      const officeOpening = setSeconds(
+        setMinutes(setHours(requestedDate, ofshour), ofsminute),
+        0
+      );
+
+      const { OFFICE_HOUR_END } = process.env;
+      const [ofehour, ofeminute] = OFFICE_HOUR_END.split(':');
+      const officeEnds = setSeconds(
+        setMinutes(setHours(requestedDate, ofehour), ofeminute),
+        0
+      );
+      if (
+        isBefore(requestedDate, officeOpening) ||
+        isAfter(requestedDate, officeEnds)
+      ) {
+        return res.status(400).json({
+          error: `New orders are allowed from ${OFFICE_HOUR_START}:00 to ${OFFICE_HOUR_END}:00`,
+        });
+      }
+
+      delivery.start_date = requestedDate;
+      delivery.save();
+    }
+
+    /**
+     * Checks if end_date is after start_date
+     */
+    if (end_date) {
+      // const startdate = parsedelivery.start_date;
+      const requestedEndDate = parseISO(end_date);
+      if (isBefore(requestedEndDate, delivery.start_date)) {
+        return res.status(400).json({
+          error: 'Cannot set end date before start date',
+        });
+      }
+    }
+
+    /**
+     * Checks for deliveryman and its availability
+     */
+    if (deliveryman_id && deliveryman_id !== delivery.deliveryman_id) {
+      const deliveryman = await Deliverymen.findByPk(deliveryman_id);
+      if (!deliveryman) {
+        return res
+          .status(404)
+          .json({ error: `Requested deliveryman not found` });
+      }
+
+      const deliveriesOfDay = await Deliveries.findAndCountAll({
+        where: {
+          deliveryman_id,
+          canceled_at: null,
+          start_date: {
+            [Op.between]: [
+              startOfDay(Number(delivery.start_date)),
+              endOfDay(Number(delivery.start_date)),
+            ],
+          },
+        },
+      });
+
+      const maximumDeliveries = process.env.MAX_DELIVERIES_DAY;
+      if (deliveriesOfDay.count >= maximumDeliveries) {
+        return res
+          .status(400)
+          .json(
+            `The requested deliveryman ${deliveryman.name} has reached the limit of ${maximumDeliveries} deliveries of the day`
+          );
+      }
+    }
+
+    delivery.update(req.body);
+
+    return res.json(delivery);
   }
 
   async delete(req, res) {
