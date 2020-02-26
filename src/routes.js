0facbe38db8f7822e4c4633c80e439ea2307d8de
Marcelo Amorim <marcelo.brasil.amorim@gmail.com>
2020-02-26 11:57:21 -0300

    Change deliveryController to ShipmentController

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import RecipientsController from './app/controllers/RecipientsController';
 import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import ShipmentsController from './app/controllers/ShipmentsController';
 import DeliveriesController from './app/controllers/DeliveriesController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -13,10 +14,29 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+/**
+ * auth
+ */
 routes.post('/sessions', SessionController.store);
+
+/**
+ * public routes from deliverymen
+ */
+routes.get(
+  '/deliveryman/:deliveryman_id/deliveries',
+  DeliveriesController.index
+);
+
+routes.put(
+  '/deliveryman/:deliveryman_id/deliveries/:delivery_id',
+  DeliveriesController.update
+);
 
 routes.use(authMiddleware);
 
+/**
+ * ADMIN only routes
+ */
 routes.get('/recipients', RecipientsController.list);
 routes.post('/recipients', RecipientsController.store);
 routes.put('/recipients/:id', RecipientsController.update);
@@ -26,10 +46,13 @@ routes.post('/deliveryman', DeliverymanController.store);
 routes.put('/deliveryman/:id', DeliverymanController.update);
 routes.delete('/deliveryman/:id', DeliverymanController.delete);
 
-routes.get('/deliveries/:id?', DeliveriesController.index);
-routes.post('/deliveries', DeliveriesController.store);
-routes.put('/deliveries/:id', DeliveriesController.update);
-routes.delete('/deliveries/:id', DeliveriesController.delete);
+/**
+ * Deliveries for admin
+ */
+routes.get('/shipments/:id?', ShipmentsController.index);
+routes.post('/shipments', ShipmentsController.store);
+routes.put('/shipments/:id', ShipmentsController.update);
+routes.delete('/shipments/:id', ShipmentsController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
